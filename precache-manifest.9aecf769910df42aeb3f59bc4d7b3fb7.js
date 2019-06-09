b9a25b678a4069343ec9b3b5655e6836f465f8be
givized <gvz45@yandex.ru>
2019-06-09 12:25:49 +0300

    Updates

---
+++ precache-manifest.9aecf769910df42aeb3f59bc4d7b3fb7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f83b47f6ca7ec7cac7ebf3378c11ced",
+    "url": "/profi-test/index.html"
+  },
+  {
+    "revision": "ff8926cbacfdf8230e35",
+    "url": "/profi-test/static/css/main.afbe291c.chunk.css"
+  },
+  {
+    "revision": "69e7b88403d277c70ea8",
+    "url": "/profi-test/static/js/2.9575b4d5.chunk.js"
+  },
+  {
+    "revision": "ff8926cbacfdf8230e35",
+    "url": "/profi-test/static/js/main.80d3c16e.chunk.js"
+  },
+  {
+    "revision": "2faf7e5ad25b46154d2a",
+    "url": "/profi-test/static/js/runtime~main.a31749cb.js"
+  },
+  {
+    "revision": "1ffd515c76b19c4c1ceb16a1cf0c7965",
+    "url": "/profi-test/static/media/1562555.1ffd515c.jpg"
+  },
+  {
+    "revision": "2a614f50000ad0c7de91bd444cce3a9e",
+    "url": "/profi-test/static/media/P7181756.jpg.optimal.2a614f50.jpg"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/profi-test/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "5d0fd7dd85c644fd5dd51f1bfe06e089",
+    "url": "/profi-test/static/media/bagarmossen.5d0fd7dd.jpg"
+  },
+  {
+    "revision": "0fa13bc0b4d7d83e72ae9e7b430ed33c",
+    "url": "/profi-test/static/media/download.0fa13bc0.jpeg"
+  },
+  {
+    "revision": "6cae008e16cac0d15bdbd3f4935a10f9",
+    "url": "/profi-test/static/media/t-centralen-liggande.6cae008e.jpeg"
+  },
+  {
+    "revision": "d549aadc74b771a02dcdc71e5b569c26",
+    "url": "/profi-test/static/media/thorildsplan-liggande.d549aadc.jpeg"
+  }
+]);
